9a994789c4548a5f17b7f8d401e8d5626a358a43
Duy Nguyen <martinnguyen0618@gmail.com>
2015-05-06 16:46:23 -0700

    fuck chen for not being here

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 //dependencies for each module used
 var express = require('express');
 var passport = require('passport');
-var InstagramStrategy = require('passport-instagram').Strategy;
-var TwitterStrategy = require('passport-twitter').Strategy;
 var http = require('http');
 var path = require('path');
-var handlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var dotenv = require('dotenv');
 var mongoose = require('mongoose');
-var Instagram = require('instagram-node-lib');
 var async = require('async');
 var app = express();
 
@@ -20,14 +16,6 @@ var models = require('./models');
 var local = false;
 //client id and client secret here, taken from .env
 dotenv.load();
-var INSTAGRAM_CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
-var INSTAGRAM_CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
-var INSTAGRAM_CALLBACK_URL = local ? 'http://localhost:3000/auth/instagram/callback' : process.env.INSTAGRAM_CALLBACK_URL;
-var REDDIT_CLIENT_ID = process.env.REDDIT_CLIENT_ID;
-var REDDIT_CLIENT_SECRET = process.env.REDDIT_CLIENT_SECRET;
-var REDDIT_CALLBACK_URL = local ? 'http://localhost:3000/auth/reddit/callback' : process.env.REDDIT_CALLBACK_URL
-Instagram.set('client_id', INSTAGRAM_CLIENT_ID);
-Instagram.set('client_secret', INSTAGRAM_CLIENT_SECRET);
 
 //connect to database
 mongoose.connect(process.env.MONGOLAB_URI);
@@ -44,69 +32,14 @@ db.once('open', function (callback) {
 //   the user by ID when deserializing.  However, since this example does not
 //   have a database of user records, the complete Instagram profile is
 //   serialized and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
 
 // Use the InstagramStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Instagram
 //   profile), and invoke a callback with a user object.
-passport.use(new InstagramStrategy({
-    clientID: INSTAGRAM_CLIENT_ID,
-    clientSecret: INSTAGRAM_CLIENT_SECRET,
-    callbackURL: INSTAGRAM_CALLBACK_URL
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-   models.User.findOne({
-    "ig_id": profile.id
-   }, function(err, user) {
-      if (err) {
-        return done(err); 
-      }
-      
-      //didnt find a user
-      if (!user) {
-        newUser = new models.User({
-          name: profile.username, 
-          ig_id: profile.id,
-          ig_access_token: accessToken
-        });
-
-        newUser.save(function(err) {
-          if(err) {
-            console.log(err);
-          } else {
-            console.log('user: ' + newUser.name + " created.");
-          }
-          return done(null, newUser);
-        });
-      } else {
-        //update user here
-        user.ig_access_token = accessToken;
-        user.save();
-        process.nextTick(function () {
-          // To keep the example simple, the user's Instagram profile is returned to
-          // represent the logged-in user.  In a typical application, you would want
-          // to associate the Instagram account with a user record in your database,
-          // and return that user instead.
-          return done(null, user);
-        });
-      }
-   });
-  }
-));
 
 
 //Configures the Template engine
-app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -114,8 +47,6 @@ app.use(cookieParser());
 app.use(session({ secret: 'keyboard cat',
                   saveUninitialized: true,
                   resave: true}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
@@ -125,188 +56,11 @@ app.set('port', process.env.PORT || 3000);
 //   the request is authenticated (typically via a persistent login session),
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { 
-    return next(); 
-  }
-  res.redirect('/login');
-}
 
-
-function ensureAuthenticatedInstagram(req, res, next) {
-  if (req.isAuthenticated() && !!req.user.ig_id) { 
-    return next(); 
-  }
-  res.redirect('/login');
-}
-
-
-//routes
-app.get('/', function(req, res){
-  res.render('login');
+//route
+app.get("*", function(req, res){
+	res.sendFile(__dirname + "/public/index.html");
 });
-
-app.get('/login', function(req, res){
-  res.render('login', { user: req.user });
-});
-
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', {user: req.user});
-});
-
-app.get('/igphotos', ensureAuthenticatedInstagram, function(req, res){
-  var query  = models.User.where({ ig_id: req.user.ig_id });
-  query.findOne(function (err, user) {
-    if (err) return err;
-    if (user) {
-      // doc may be null if no document matched
-      Instagram.users.liked_by_self({
-        access_token: user.ig_access_token,
-        complete: function(data) {
-          console.log(data);
-          //Map will iterate through the returned data obj
-          var imageArr = data.map(function(item) {
-            //create temporary json object
-            tempJSON = {};
-            tempJSON.url = item.images.low_resolution.url;
-            //insert json object into image array
-            return tempJSON;
-          });
-          res.render('photos', {photos: imageArr});
-        }
-      }); 
-    }
-  });
-});
-
-app.get('/igFollowers', ensureAuthenticatedInstagram, function(req, res) {
-  var query = models.User.where({ig_id : req.user.ig_id});
-
-  query.findOne(function(err, user) {
-    if(err) return err;
-    if(user) {
-      Instagram.users.followed_by({
-        user_id : user.ig_id,
-        access_token : user.ig_access_token,
-        complete : function(data) {
-          //console.log(data);
-          var asyncTasks = [];
-          var mediaCounts = [];
-
-          data.forEach(function(item) {
-            asyncTasks.push(function(callback) {
-              Instagram.users.info({
-                user_id : item.id,
-                access_token : user.ig_access_token,
-                complete : function(data) {
-                  mediaCounts.push(data);
-                  callback();
-                },
-                error : function(errorMessage, errorObject, caller) {
-                  console.log(errorMessage, errorObject);
-                  callback();
-                }
-              });
-            });
-          });
-
-          async.parallel(asyncTasks, function(err) {
-            if(err) return console.log(err);
-            console.log('done');
-            return res.json({ users : mediaCounts });
-          });
-        },
-        error : function(errorMessage, errorObject, caller) {
-          console.log(errorMessage, errorObject);
-        }
-      });
-    }
-  });
-});
-
-app.get('/igMediaCounts', ensureAuthenticatedInstagram, function(req, res){
-  var query  = models.User.where({ ig_id: req.user.ig_id });
-  query.findOne(function (err, user) {
-    if (err) return err;
-    if (user) {
-      Instagram.users.follows({ 
-        user_id: user.ig_id,
-        access_token: user.ig_access_token,
-        complete: function(data) {
-          console.log(data);
-          // an array of asynchronous functions
-          var asyncTasks = [];
-          var mediaCounts = [];
-           
-          data.forEach(function(item){
-            asyncTasks.push(function(callback){
-              // asynchronous function!
-              Instagram.users.info({ 
-                  user_id: item.id,
-                  access_token: user.ig_access_token,
-                  complete: function(data) {
-                    mediaCounts.push(data);
-                    callback();
-                  }
-                });            
-            });
-          });
-          
-          // Now we have an array of functions, each containing an async task
-          // Execute all async tasks in the asyncTasks array
-          async.parallel(asyncTasks, function(err){
-            // All tasks are done now
-            if (err) return err;
-            return res.json({users: mediaCounts});        
-          });
-        }
-      });   
-    }
-  });
-});
-
-app.get('/visualization', ensureAuthenticatedInstagram, function (req, res){
-  res.render('visualization', { user : req.user });
-}); 
-
-app.get('/myvisualization', ensureAuthenticated, function(req, res) {
-  res.render('myvisualization', { user : req.user });
-});
-
-app.get('/myc3visualization', ensureAuthenticated, function(req, res) {
-  res.render('myc3visualization', { user : req.user });
-});
-
-
-app.get('/c3visualization', ensureAuthenticatedInstagram, function (req, res){
-  res.render('c3visualization', { user : req.user });
-}); 
-
-app.get('/auth/instagram',
-  passport.authenticate('instagram'),
-  function(req, res){
-    // The request will be redirected to Instagram for authentication, so this
-    // function will not be called.
-  });
-
-app.get('/auth/instagram/callback', 
-  passport.authenticate('instagram', { failureRedirect: '/login'}),
-  function(req, res) {
-    res.redirect('/account');
-  });
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-app.get('/auth/reddit', passport.authenticate('reddit'), function(req, res) {
-
-});
-
-app.get('/auth/reddit/callback', passport.authenticate('reddit', { failureRedirect : '/login'}), function(req, res) {
-  res.redirect('account');
-} );
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
